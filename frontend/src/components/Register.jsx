d622af36244c4721a9b9c151e0a54f08274bc629
Tom <burchard92@outlook.de>
2025-05-23 15:11:00 +0000

    MDBootstrap in Register angepasst und Bild eingefügt.

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,17 @@
 import { Form, Button } from "react-bootstrap";
 import { useState } from "react";
-
+import pizzaImage from '../assets/pizza.png'
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBInput
+}
+from 'mdb-react-ui-kit';
 
 
 function Register() {
@@ -16,11 +27,14 @@ function Register() {
 
     const handleChange = (e) => {
         const { name, value } = e.target;
+        console.log(formData);
         setFormData( (prev) => ({
             ...prev,
             [name]: value,
         }));
+        console.log(formData);
     };
+
 
 
     const handleSubmit = async (e) => {
@@ -34,13 +48,15 @@ function Register() {
                 headers: { "Content-Type": "application/json", },
                 body: JSON.stringify(formData),
             });
-        
+
+            const jsonData = await res.json();
 
         if(res.ok) {
             alert("Sie haben sich erfolgreich Registriert.");
+            console.log("jsonData: ", jsonData);
         } else {
-            const errorData = await res.json();
-            alert("Fehler bei der Registrierung.");
+        
+            console.log("Fehler bei der Anmeldung", jsonData.message);
         }
         } catch(err) {
             console.error("Fehler: ", err);
@@ -50,82 +66,112 @@ function Register() {
 
     return(
         <>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3">
-            <Form.Label>Benutzername</Form.Label>
-            <Form.Control
-                type="text"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-                placeholder="Benutzename eingeben"
-                required
-            />
-            </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Label>Vorname</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="firstname"
-                    value={formData.firstname}
-                    onChange={handleChange}
-                    placeholder="Vorname eingeben"
-                    required
-                />
-            </Form.Group>
+<MDBContainer fluid>
+    <form onSubmit={handleSubmit}>
 
-            <Form.Group className="mb-3">
-                <Form.Label>Nachname</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="lastname"
-                    value={formData.lastname}
-                    onChange={handleChange}
-                    placeholder="Nachname eingeben"
-                    required
-                />
-            </Form.Group>
+        <MDBCard className='text-black m-5' style={{borderRadius: '25px'}}>
+            <MDBCardBody>
+            <MDBRow>
+                <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
 
-            <Form.Group className="mb-3">
-                <Form.Label>E-Mail</Form.Label>
-                <Form.Control
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    placeholder="E-Mail eingeben"
-                    required
-                />
-            </Form.Group>
+                <p className="text-center h2 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registrier dich!</p>
 
-            <Form.Group className="mb-3">
-                <Form.Label>Passwort</Form.Label>
-                <Form.Control
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    placeholder="Passwort eingeben"
-                    required
-                />
-            </Form.Group>
+                <div className="d-flex flex-row gap-3">
+                    <div className="d-flex flex-row align-items-center mb-4 ">
 
-            <Form.Group className="mb-3">
-                <Form.Label>Passwort bestätigen</Form.Label>
-                <Form.Control
-                    type="password"
-                    name="passwordCheck"
-                    value={formData.passwordCheck}
-                    onChange={handleChange}
-                    placeholder="Passwort erneut eingeben"
-                    required
-                />
-</Form.Group>
+                        <MDBInput label='Benutzername' id='form1' type='text' className='w-100'
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            placeholder="Benutzename eingeben"
+                            required/>
+                    </div>
 
-        <Button type="submit" variant="primary">Registrieren</Button>
+                    <div className="d-flex flex-row align-items-center mb-4 ">
+                        
+                        <MDBInput label='E-Mail' id='form2' type='email'
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            placeholder="E-Mail eingeben"
+                            required/>
+                    </div>
+                </div>
 
-        </Form>
+                <div className="d-flex flex-row gap-3">
+                    <div className="d-flex flex-row align-items-center mb-4 ">
+                        
+                        <MDBInput label='Vorname' id='form3' type='text' className='w-100'
+                            name="firstname"
+                            value={formData.firstname}
+                            onChange={handleChange}
+                            placeholder="Vorname eingeben"
+                            required/>
+                    </div>
+
+                    <div className="d-flex flex-row align-items-center mb-4">
+                        
+                        <MDBInput label='Nachname' id='form4' type='text' className='w-100'
+                            name="lastname"
+                            value={formData.lastname}
+                            onChange={handleChange}
+                            placeholder="Nachname eingeben"
+                            required/>
+                    </div>
+                </div>
+
+                <div className="d-flex flex-row gap-3" >
+                    <div className="d-flex flex-row align-items-center mb-4">
+                        <MDBInput label='Passwort' id='form5' type='password'
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            placeholder="Passwort eingeben"
+                            required/>
+                    </div>
+
+                    <div className="d-flex flex-row align-items-center mb-4">
+                        <MDBInput label='Passwort wiederholen' id='form6' type='password'
+                            name="passwordCheck"
+                            value={formData.passwordCheck}
+                            onChange={handleChange}
+                            placeholder="Passwort erneut eingeben"
+                            required/>
+                    </div>
+                </div>
+                
+                <div className="w-100">
+                    <Button type="submit" 
+                            className="w-100 d-flex align-items-center justify-content-center" 
+                            style={{
+                                userSelect: 'none',
+                                whiteSpace: 'nowrap',
+                                height: '40px',
+                                transition: 'none',      // kein Übergang beim Klick
+                                boxShadow: 'none',       // kein Schatten beim Klicken
+                                outline: 'none',          // kein Outline beim Klicken
+                                transform: 'none'
+                            }}>Registrieren
+                    </Button>
+                    <p className="small fw-bold mt-2 pt-1 mb-2">Bereits Registriert? <a href="/login" className="link-danger">Login</a></p>
+                </div>
+                </MDBCol>
+
+
+                <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+                <MDBCardImage src={pizzaImage} fluid/>
+                </MDBCol>
+
+            </MDBRow>
+            </MDBCardBody>
+        </MDBCard>
+    </form>
+
+    </MDBContainer>
+
+
+
         </>
     );
 }
