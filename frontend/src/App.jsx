ca83434ffb5c1a7ad2673ab4618a11db04e93382
Tom <burchard92@outlook.de>
2025-05-27 16:08:21 +0000

    Funktionalität zum speichern und leeren des usernames hinzugefügt. Style der Form angepasst und automatisches schließen vom "Burgermenue" implementiert.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
+import Rezept from './components/Rezept';
 
 
 
@@ -33,10 +34,13 @@ function App() {
     useEffect( () => {
         const storedToken = localStorage.getItem('token');
         const storedUserId = localStorage.getItem('userID');
+        const storedUserName = localStorage.getItem('username');
         
         if( storedToken && storedUserId ) {
         setIsLoggedIn(true);
         setUserId(storedUserId);
+        if( storedUserName ) 
+            setUserName(storedUserName);
         }
     }, [navigate]
     );
@@ -44,7 +48,9 @@ function App() {
     const handleLogout = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('userID');
+        localStorage.removeItem('username');
         setIsLoggedIn(false);
+        setUserName('');
         navigate('/');
     };
 
@@ -53,26 +59,43 @@ function App() {
         <>
         <MDBNavbar className='fixed-top shadow-3' expand='lg' light bgColor='primary'>
         <MDBContainer fluid>
-            <MDBNavbarBrand className='kochbuch text-white fw-bold ' style={{ fontFamily: 'DancingScript, cursive' }}>Kochbuch</MDBNavbarBrand>
-            <MDBNavbarToggler className='text-white'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
-            onClick={() => setOpenNavSecond(!openNavSecond)}
+            <MDBNavbarBrand 
+                className='kochbuch text-white fw-bold ' 
+                style={{ 
+                    fontFamily: 'DancingScript, cursive',
+                    fontSize: '2rem'
+                }}
+                >
+                Kochbuch
+            </MDBNavbarBrand>
+            <MDBNavbarToggler 
+                className='text-white'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+                onClick={() => setOpenNavSecond(!openNavSecond)}
+                style={{
+                    fontSize: '1.5rem',      // größerer Icon
+                    padding: '1rem',       // größerer Klickbereich
+                    backgroundColor: 'rgba(255,255,255,0.2)', // leicht sichtbarer Hintergrund
+                    borderRadius: '8px',      // runde Ecken
+                    border: '2px solid white',  // <- dauerhafte weiße Umrandung
+                    borderRadius: '8px',
+                    color: 'white'
+                }}
             >
-            <MDBIcon icon='bars' fas />
+            <MDBIcon icon='bars' fas  />
             </MDBNavbarToggler>
             <MDBCollapse navbar open={openNavSecond}>
-            <MDBNavbarNav>
+            <MDBNavbarNav className='link-style'>
             
-
-                <Nav.Link className="text-white" as={Link} to="/">Home</Nav.Link>
-                <Nav.Link className="text-white" as={Link} to="/register">Registrierung</Nav.Link>
-                { isLoggedIn ?
-                    <Nav.Link className="text-white" onClick={handleLogout}>Logout</Nav.Link>
-                    :
-                    <Nav.Link className="text-white" as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link className="text-white" as={Link} to="/" onClick={() => setOpenNavSecond(false)}>Home</Nav.Link>
+                <Nav.Link className="text-white" as={Link} to="/register" onClick={() => setOpenNavSecond(false)}>Registrierung</Nav.Link>
+                <Nav.Link className="text-white" as={Link} to="/rezept" onClick={() => setOpenNavSecond(false)}>Rezept</Nav.Link>
+                { isLoggedIn 
+                    ? <Nav.Link className="text-white"onClick={() => { handleLogout(); setOpenNavSecond(false); }}>Logout</Nav.Link>
+                    : <Nav.Link className="text-white" as={Link} to="/login" onClick={() => setOpenNavSecond(false)}>Login</Nav.Link>
                 }
-
+            
 
             </MDBNavbarNav>
             </MDBCollapse>
@@ -81,9 +104,11 @@ function App() {
 
         <Routes>
         <Route path='/' element={<Home
+                        isLoggedIn = {isLoggedIn}
                         userName={userName} />}  />
         
         <Route path='/register' element={<Register/>}  />
+        <Route path='/rezept' element={<Rezept/>}  />
 
         <Route path='/login' element={<Login
             isLoggedIn = {isLoggedIn}
@@ -91,6 +116,7 @@ function App() {
             setUserName = {setUserName}
             userName = {userName} 
         />}  />
+
         </Routes>
         </>
     );
