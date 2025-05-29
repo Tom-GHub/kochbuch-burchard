2158d775b8ba26a4de939aa4579925eb2279a6f3
Tom <burchard92@outlook.de>
2025-05-29 07:50:02 +0000

    middleware zur Authentifizierung erstellt.

---
+++ backend/middleware/auth.js
@@ -0,0 +1,27 @@
+
+
+// token wird gelese, geprüft - inhalt wird als req auf die variable gelegt
+
+
+// aus req.user kann ich mit req.user.userID die id auslesen (passiert in der rezept.js mit dem auslesen)
+
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1]; // was genau macht diese zeiel? 
+
+    if (!token) {
+        return res.status(401).json({ error: 'Nicht autorisiert' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(403).json({ error: 'Token ungültig' });
+    }
+};
+
+export default authMiddleware;
