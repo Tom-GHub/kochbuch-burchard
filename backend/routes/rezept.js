7af252a0cabd68780404f9497e11c53e5ebd6c80
Tom <burchard92@outlook.de>
2025-05-29 07:52:43 +0000

    middleware in rezept.js implementiert und try-catch-block für Fehlerbehandlung eingebaut.

--- backend/routes/rezept.js
+++ backend/routes/rezept.js
@@ -1,28 +1,36 @@
 import express from 'express';
 import getDatabaseConnection from '../db.js';
 import 'dotenv/config';
+import authMiddleware from '../middleware/auth.js';
+import multer from 'multer';
 
+// const upload = multer({ dest: 'public/uploads/' });        in router.post()-> upload.single('image'),     const imageFilename = req.file?.filename ?? null;   
 const router = express.Router(); //erstellt route objekt
 
-router.post('/api/rezept', async (req, res) => {
+// middleware wird in zeile 8 mit eingefügt -> damit zugriff auf req.user (user ist im token in der login.js)
+router.post('/api/rezept', authMiddleware, async (req, res) => {
     console.log("rezept.js - req.body: ", req.body);
     
-    const { user_id, titel, zutatenliste, zubereitung, imageUpload } = req.body;
-
+    const user_id = req.user.id;
+    const { titel, zutatenliste, zubereitung, imageUpload } = req.body;
+    
     const conn = await getDatabaseConnection();
 
-    await conn.query(
-        `INSERT INTO recipe (user_id, title, ingredients, instructions, image)
-        VALUES (?, ?, ?, ?, ?)`,
-        [user_id, titel, zutatenliste, zubereitung, imageUpload]
-    );
-
+    try {
+        await conn.query(
+            `INSERT INTO recipe (user_id, title, ingredients, instructions, image)
+            VALUES (?, ?, ?, ?, ?)`,
+            [user_id, titel, zutatenliste, zubereitung, imageUpload]
+        );
+        res.json({ message: 'Rezept gespeichert' });
+    } catch (err) {
+        console.error('Fehler beim Aktualisieren des Rezeptes:', err);
+        res.status(500).json({ err: 'Fehler beim Aktualisieren des Rezeptes' });
+    } finally {
+        conn.release();
+    }
 
     res.status(200);
 });
-
-
-
-
 
 export default router;
