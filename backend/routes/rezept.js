ac60933975037cd031ab421fd83fcdc40bf25a47
Tom <burchard92@outlook.de>
2025-05-30 12:37:17 +0000

    backend: multer für  das Interpretieren des Bildes verwendet. Variable zum Speichern des Bildes erstellt und SQL-Abfrage anbepasst.

--- backend/routes/rezept.js
+++ backend/routes/rezept.js
@@ -4,15 +4,17 @@ import 'dotenv/config';
 import authMiddleware from '../middleware/auth.js';
 import multer from 'multer';
 
-// const upload = multer({ dest: 'public/uploads/' });        in router.post()-> upload.single('image'),     const imageFilename = req.file?.filename ?? null;   
+const upload = multer({ dest: 'public/uploads/' });
 const router = express.Router(); //erstellt route objekt
 
 // middleware wird in zeile 8 mit eingefügt -> damit zugriff auf req.user (user ist im token in der login.js)
-router.post('/api/rezept', authMiddleware, async (req, res) => {
+router.post('/api/rezept', authMiddleware, upload.single('picture'), async (req, res) => {
+    
     console.log("rezept.js - req.body: ", req.body);
     
     const user_id = req.user.id;
-    const { titel, zutatenliste, zubereitung, imageUpload } = req.body;
+    const { titel, zutatenliste, zubereitung } = req.body;
+    const picture = req.file ? req.file.filename : null;
     
     const conn = await getDatabaseConnection();
 
@@ -20,17 +22,18 @@ router.post('/api/rezept', authMiddleware, async (req, res) => {
         await conn.query(
             `INSERT INTO recipe (user_id, title, ingredients, instructions, image)
             VALUES (?, ?, ?, ?, ?)`,
-            [user_id, titel, zutatenliste, zubereitung, imageUpload]
+            [user_id, titel, zutatenliste, zubereitung, picture]
         );
         res.json({ message: 'Rezept gespeichert' });
     } catch (err) {
-        console.error('Fehler beim Aktualisieren des Rezeptes:', err);
-        res.status(500).json({ err: 'Fehler beim Aktualisieren des Rezeptes' });
+        console.error('Fehler beim Speichern des Rezeptes:', err);
+        res.status(500).json({ err: 'Fehler beim Speichern des Rezeptes' });
     } finally {
         conn.release();
     }
 
-    res.status(200);
 });
+
+
 
 export default router;
